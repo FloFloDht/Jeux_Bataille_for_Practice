81a4a575dcac8e8aa38ece3c4631e9b9247f5eda
FlorentDht <43930524+FloFloDht@users.noreply.github.com>
2021-10-06 00:19:40 +0200

    constructeur + getter et setter de Deck

--- src/flo/game/Deck.java
+++ src/flo/game/Deck.java
@@ -3,5 +3,19 @@ package flo.game;
 import java.util.ArrayList;
 
 public class Deck {
-    ArrayList deck = new ArrayList();
+
+    //paquet de carte, contient un total de 20 cartes
+    ArrayList<Card> deck = new ArrayList<>();
+
+    public Deck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
 }
